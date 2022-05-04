7520e003c5929d7b39791918d474557d34901352
Nhựt Thanh <86595159+thanhcuppi1@users.noreply.github.com>
2022-05-04 17:26:59 +0700

    Add Main menu, track select

---
+++ Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour {
+
+	public void PlayGame () {
+		SceneManager.LoadScene (2);
+	}
+
+	public void TrackSelect () {
+		SceneManager.LoadScene (1);
+	}
+
+	public void MainMenu () {
+		SceneManager.LoadScene (0);
+	}
+
+
+
+	public void Track01 () {
+		SceneManager.LoadScene (2);
+	}
+}
--- Assets/Scripts/LapComplete.cs
+++ Assets/Scripts/LapComplete.cs
@@ -18,6 +18,15 @@ public class LapComplete : MonoBehaviour {
 	public int LapsDone;
 	public float RawTime;
 
+	public GameObject RaceFinish;
+
+
+	void Update(){
+		if (LapsDone == 3) {
+			RaceFinish.SetActive (true);
+		}
+	}
+
 	void OnTriggerEnter () {
 		LapsDone += 1;
 		RawTime = PlayerPrefs.GetFloat ("RawTime");
