d300f4b9bce250e2ed2440914ab58f7ecb61768b
Nhựt Thanh <86595159+thanhcuppi1@users.noreply.github.com>
2022-05-05 10:06:53 +0700

    fix minimap

--- Assets/Scripts/ButtonOptions.cs
+++ Assets/Scripts/ButtonOptions.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ButtonOptions : MonoBehaviour {
 
-	public void PlayGame () {
-		SceneManager.LoadScene (2);
-	}
+	//public void PlayGame () {
+		//SceneManager.LoadScene (2);
+	//}
 
-	public void TrackSelect () {
+	public void PlayGame () {
 		SceneManager.LoadScene (1);
 	}
 
---
+++ Assets/Scripts/GlobalCar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCar : MonoBehaviour {
+
+	public static int CarType;
+	public GameObject TrackWindown;
+
+	public void RedCar () {
+		CarType = 1;
+		TrackWindown.SetActive (true);
+	}
+
+	public void BlueCar () {
+		CarType = 2;
+		TrackWindown.SetActive (true);
+	}
+}
